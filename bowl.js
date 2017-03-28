a6b2fcba28cbedbf3454c6e7ee8243e42de6d2fd
katie hoffman <kathryn.hoffman@detroitlabs.com>
2017-03-28 15:00:50 -0400

    Adding user name validation and pairing down functions

--- bowl.js
+++ bowl.js
@@ -1,16 +1,13 @@
 var readline = require('readline');
-
 var prompt = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
 var Frame = function(firstRoll, secondRoll, score) {
-	this.firstRoll = firstRoll;
-	this.secondRoll = secondRoll;
-	this.score= score;
+    this.firstRoll = firstRoll;
+    this.secondRoll = secondRoll;
+    this.score= score;
 };
-
 var allRounds = [];
 var playerOneName;
 var roll1;
@@ -18,88 +15,86 @@ var roll2;
 var rollToPush;
 var frameScore = 0;
 var overallScore=0;
-
 var firstRoll = function() {
-	prompt.question("Are you ready to roll, " + playerOneName + "?" , (rollPlz) => {
-		if (rollPlz == "roll") {
-			console.log("Rolling now!");
-			roll1 = Math.floor(Math.random() * 10) + 1;
-				if (roll1 == 10) {
-					console.log("Strike!!!");
-					roll2 = 0;
-					strikeScore();
-					endTurn();
-				} else {
-			console.log("Your 1st roll: " + roll1); 
-			secondRoll();
-			}
-		} else {
-			firstRoll();
-		}
-	});
+    prompt.question("Are you ready to roll, " + playerOneName + "? " , (rollPlz) => {
+        if (rollPlz == "roll") {
+            console.log("Rolling now!");
+            roll1 = 10;
+                if (roll1 == 10) {
+                    console.log("Strike!!!");
+                    roll2 = 0;
+                    strikeScore();
+                    endTurn();
+                } else {
+            console.log("Your 1st roll: " + roll1); 
+            secondRoll();
+            }
+        } else {
+            firstRoll();
+        }
+    });
 };
-
 var secondRoll = function() {
-	prompt.question("Are you ready to roll again, " + playerOneName + "?" , (rollPlz2) => {
-		if (rollPlz2 == "roll") {
-			console.log("Rolling now!");
-			roll2 = Math.floor(Math.random() * (10-roll1+1));
-			console.log("Your 2nd roll: " + roll2);
-			frameScore = roll1+roll2;
-
-			if (frameScore==10) {
-				spareScore();
-			}
-			endTurn();
-		} else {
-			secondRoll();
-		}
-	});
+    prompt.question("Are you ready to roll again, " + playerOneName + "? " , (rollPlz2) => {
+        if (rollPlz2 == "roll") {
+            console.log("Rolling now!");
+            roll2 = Math.floor(Math.random() * (10-roll1+1));
+            console.log("Your 2nd roll: " + roll2);
+            frameScore = roll1+roll2;
+            if (frameScore==10) {
+                spareScore();
+            }
+            endTurn();
+            } else {
+            secondRoll();
+        }
+    });
 };
-
 var strikeScore = function() {
-	frameScore=30;
-	
+    frameScore=30;
 };
-
 var spareScore = function() {
-	frameScore=20;
-	console.log("You got a SPARE!!!");
+    frameScore=20;
+    console.log("You got a SPARE!!!");
 };
-
 var scoreTotal = function() {
-	for (var i =0; i<allRounds.length; i++) {
-		overallScore = overallScore + allRounds[i].score;
-	}
-	console.log(overallScore);
+    for (var i =0; i<allRounds.length; i++) {
+        overallScore = overallScore + allRounds[i].score;
+    }
+    console.log(overallScore);
 };
-
 var endTurn = function() {
-	console.log("Your rolls: " + roll1 + " " + roll2);
-	rollToPush = new Frame(roll1, roll2, frameScore);
-	allRounds.push(rollToPush);
-
-	console.log(allRounds);
-	console.log(overallScore);
-	
-	checkFrameLength();
+    console.log("Your rolls: " + roll1 + " " + roll2);
+    rollToPush = new Frame(roll1, roll2, frameScore);
+    allRounds.push(rollToPush);
+    console.log(allRounds);
+    checkFrameLength();
 };
-
-prompt.question("What is your name?", (inputP1Name) => {
-	playerOneName = inputP1Name;
-	firstRoll();
+var validateName = function(x) {
+    if (x.length > 1 && x.length < 16) {
+        return x;
+    } else {
+        console.log("Please use 2-15 characters");
+        askName();
+    }
+};
+var askName = function(){
+    prompt.question("What is your name? ", (inputP1Name) => {
+    playerOneName = validateName(inputP1Name);
+    firstRoll();
 });
+};
 //check for amount in array, if == 10, then go to end game to quit
 var checkFrameLength = function() {
-	if (allRounds.length ==10) {
-		endGame();
-	}
-	else {
-		firstRoll();
-	}
-}
+    if (allRounds.length ==10) {
+        endGame();
+    }
+    else {
+        firstRoll();
+    }
+};
 var endGame = function() {
-	scoreTotal();
+    scoreTotal();
 };
 var sleep = function(milliseconds) {
     var start = new Date().getTime();
@@ -109,3 +104,4 @@ var sleep = function(milliseconds) {
       }
     }
 };
+askName();
