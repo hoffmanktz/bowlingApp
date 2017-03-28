c675c0b830a97483ca5d66559540d46f6e6e678d
katie hoffman <kathryn.hoffman@detroitlabs.com>
2017-03-28 11:48:16 -0400

    Breaking down individual functions

--- bowl.js
+++ bowl.js
@@ -7,14 +7,22 @@ var prompt = readline.createInterface({
 
 var allRounds = [];
 var playerOneName;
+var roll1;
+var roll2;
 
 var firstRoll = function() {
 	prompt.question("Are you ready to roll, " + playerOneName + "?" , (rollPlz) => {
 		if (rollPlz == "roll") {
 			console.log("Rolling now!");
-			var roll1 = Math.floor(Math.random() * 10) + 1;
+			roll1 = Math.floor(Math.random() * 10) + 1;
+				if (roll1 == 10) {
+					console.log("Strike!!!");
+					roll2 = 0;
+					endTurn();
+				} else {
 			console.log("Your 1st roll: " + roll1); 
 			secondRoll();
+			}
 		} else {
 			firstRoll();
 		}
@@ -25,12 +33,19 @@ var secondRoll = function() {
 	prompt.question("Are you ready to roll again, " + playerOneName + "?" , (rollPlz2) => {
 		if (rollPlz2 == "roll") {
 			console.log("Rolling now!");
-			var roll2 = Math.floor(Math.random() * 10) + 1;
+			roll2 = Math.floor(Math.random() * (10-roll1));
 			console.log("Your 2nd roll: " + roll2);
+			endTurn();
 		} else {
 			secondRoll();
 		}
 	});
+};
+
+var endTurn = function() {
+
+	console.log("Your rolls: " + roll1 + " " + roll2);
+
 };
 
 prompt.question("What is your name?", (inputP1Name) => {
